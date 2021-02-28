3b7d9278a57e4c940c6609b82611a32e80295aa2
AndBauer70 <79770216+AndBauer70@users.noreply.github.com>
2021-02-28 15:27:37 +0100

    WM360

---
+++ data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010018",
+      "name": "R0010018",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1198038130522043,
+          "pitch": 0.11175614266861089,
+          "rotation": 0.7853981633974483,
+          "target": "1-r0010024"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8214462714240156,
+          "pitch": -1.8264646683974206e-8,
+          "title": "Aussichtspunkt 1",
+          "text": "Hallo, schön dass ihr da seit. Ihr, die Schülerinnen und Schüler der 6b seit jetzt vom Parkplatz aus ein mehrere 100 Meter in ein ehemaliges Moor hineingelaufen.&nbsp;<div><br></div><div><br></div>"
+        },
+        {
+          "yaw": 2.6727264059117584,
+          "pitch": 0.05528153302403993,
+          "title": "Ehemaliges Moor?",
+          "text": "Ganz recht. Der Bereich in dem ihr euch jetzt gerade befindet ist von uns Menschen intensiv genutzt worden. Durch Gräben wurde das Moor entwässert und wenn es trocken genug war die Torfschicht abgebaut. Die kann mehrer Meter mächtig gewesen sein. Auch wurde stellenweise Ortstein abgebaut, eine Bodenschicht, die viel Eisenoxid enthält und so zur Stahlherstellung genutzt wurde.&nbsp;"
+        },
+        {
+          "yaw": 3.099919854753484,
+          "pitch": -0.035842293897751176,
+          "title": "Hier wächst je kein Baum und kein Strauch?",
+          "text": "Das ist richtig und gewollt. Um dem Moor die Chance zu geben, sich wieder aufzubauen muss es wiedervernässt werden. Bäume wie die Birken hinter mir verdunsten über ihre Blätter viel Wasser, so dass es ungünstig für die Renaturierung des Moores ist, wenn dort Bäume oder Sträucher stehen. Das ist der Grund dafür, dass Schafe über die Fläche getrieben werden, denn die fressen gerne die jungen Bäume und Sträucher, da sie so schön saftig grün sind.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010024",
+      "name": "R0010024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4823842708746966,
+          "pitch": 0.32612949707634264,
+          "rotation": 5.497787143782138,
+          "target": "2-r0010054"
+        },
+        {
+          "yaw": 2.866288631247185,
+          "pitch": 0.14346006575124015,
+          "rotation": 5.497787143782138,
+          "target": "0-r0010018"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010054",
+      "name": "R0010054",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.3207318688045984,
+          "pitch": 0.057205613137112366,
+          "title": "länger renaturiert",
+          "text": "Hier seht ihr auf den ersten Blick, dass hier viel Wasser zu sehen ist, also eine sehr feuchte Fläche vorhanden ist. Das Wasser ist angestaut, so dass in ihm die Torfmosse wachsen können."
+        },
+        {
+          "yaw": -0.8684889943764382,
+          "pitch": 0.18533234373495944,
+          "title": "Der Torfwall",
+          "text": "Dieser Wall aus Torf trennt die rechte,höher gelegene Fläche von der rechten, niedriger gelegenen Fläche. Er schützt davor, dass das Wasser in den niedrigeren Bereich fließt."
+        }
+      ]
+    }
+  ],
+  "name": "WildesMoor",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
