808301529959ee32dd63491fbe6d0f0182129e57
AndBauer70 <79770216+AndBauer70@users.noreply.github.com>
2021-03-01 09:40:31 +0100

    AB
    Neue Hotspots

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-r0010018",
-      "name": "R0010018",
+      "id": "0-standpunkt-1",
+      "name": "Standpunkt 1",
       "levels": [
         {
           "tileSize": 256,
@@ -30,80 +30,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.1198038130522043,
-          "pitch": 0.11175614266861089,
-          "rotation": 0.7853981633974483,
-          "target": "1-r0010024"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.8214462714240156,
-          "pitch": -1.8264646683974206e-8,
-          "title": "Aussichtspunkt 1",
-          "text": "Hallo, schön dass ihr da seit. Ihr, die Schülerinnen und Schüler der 6b seit jetzt vom Parkplatz aus ein mehrere 100 Meter in ein ehemaliges Moor hineingelaufen.&nbsp;<div><br></div><div><br></div>"
+          "yaw": -1.230928848955564,
+          "pitch": 0.020712722811211393,
+          "rotation": 0,
+          "target": "3-torfschicht"
         },
         {
-          "yaw": 2.6727264059117584,
-          "pitch": 0.05528153302403993,
-          "title": "Ehemaliges Moor?",
-          "text": "Ganz recht. Der Bereich in dem ihr euch jetzt gerade befindet ist von uns Menschen intensiv genutzt worden. Durch Gräben wurde das Moor entwässert und wenn es trocken genug war die Torfschicht abgebaut. Die kann mehrer Meter mächtig gewesen sein. Auch wurde stellenweise Ortstein abgebaut, eine Bodenschicht, die viel Eisenoxid enthält und so zur Stahlherstellung genutzt wurde.&nbsp;"
+          "yaw": 1.9522617057690734,
+          "pitch": 0.00010079398469287071,
+          "rotation": 4.71238898038469,
+          "target": "8-verbindungsweg"
         },
         {
-          "yaw": 3.099919854753484,
-          "pitch": -0.035842293897751176,
-          "title": "Hier wächst je kein Baum und kein Strauch?",
-          "text": "Das ist richtig und gewollt. Um dem Moor die Chance zu geben, sich wieder aufzubauen muss es wiedervernässt werden. Bäume wie die Birken hinter mir verdunsten über ihre Blätter viel Wasser, so dass es ungünstig für die Renaturierung des Moores ist, wenn dort Bäume oder Sträucher stehen. Das ist der Grund dafür, dass Schafe über die Fläche getrieben werden, denn die fressen gerne die jungen Bäume und Sträucher, da sie so schön saftig grün sind.&nbsp;"
-        }
-      ]
-    },
-    {
-      "id": "1-r0010024",
-      "name": "R0010024",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1680,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.4823842708746966,
-          "pitch": 0.32612949707634264,
-          "rotation": 5.497787143782138,
-          "target": "2-r0010054"
-        },
-        {
-          "yaw": 2.866288631247185,
-          "pitch": 0.14346006575124015,
-          "rotation": 5.497787143782138,
-          "target": "0-r0010018"
+          "yaw": 0.9215928211405622,
+          "pitch": 0.4208866252267214,
+          "rotation": 0,
+          "target": "6-wasser"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-r0010054",
-      "name": "R0010054",
+      "id": "1-schutzhtte",
+      "name": "Schutzhütte",
       "levels": [
         {
           "tileSize": 256,
@@ -129,27 +78,368 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": [
+      "linkHotspots": [
         {
-          "yaw": -2.3207318688045984,
-          "pitch": 0.057205613137112366,
-          "title": "länger renaturiert",
-          "text": "Hier seht ihr auf den ersten Blick, dass hier viel Wasser zu sehen ist, also eine sehr feuchte Fläche vorhanden ist. Das Wasser ist angestaut, so dass in ihm die Torfmosse wachsen können."
+          "yaw": 2.8527810201890844,
+          "pitch": 0.16315744654385433,
+          "rotation": 5.497787143782138,
+          "target": "0-standpunkt-1"
         },
         {
-          "yaw": -0.8684889943764382,
-          "pitch": 0.18533234373495944,
-          "title": "Der Torfwall",
-          "text": "Dieser Wall aus Torf trennt die rechte,höher gelegene Fläche von der rechten, niedriger gelegenen Fläche. Er schützt davor, dass das Wasser in den niedrigeren Bereich fließt."
+          "yaw": 0.5339856984515698,
+          "pitch": 0.36040148586060283,
+          "rotation": 5.497787143782138,
+          "target": "2-torfwall"
+        },
+        {
+          "yaw": -1.3929916490186791,
+          "pitch": -0.0702581684742043,
+          "rotation": 5.497787143782138,
+          "target": "5-birke"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-torfwall",
+      "name": "Torfwall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6786234515586678,
+          "pitch": 0.04810510051606087,
+          "rotation": 11.780972450961727,
+          "target": "4-torfwallweg"
+        },
+        {
+          "yaw": 1.7693464007492032,
+          "pitch": 0.2722842353763646,
+          "rotation": 7.0685834705770345,
+          "target": "0-standpunkt-1"
+        },
+        {
+          "yaw": 2.1120706866146346,
+          "pitch": 0.08328050271180842,
+          "rotation": 13.351768777756625,
+          "target": "1-schutzhtte"
+        },
+        {
+          "yaw": 2.7283863649646865,
+          "pitch": -0.13613646477510954,
+          "rotation": 0,
+          "target": "5-birke"
+        },
+        {
+          "yaw": -1.5518414071341553,
+          "pitch": 0.33307751491678417,
+          "rotation": 5.497787143782138,
+          "target": "7-renaturierung"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-torfschicht",
+      "name": "Torfschicht",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0436464352258206,
+          "pitch": -0.00003682688048733951,
+          "rotation": 4.71238898038469,
+          "target": "0-standpunkt-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.14051788600035842,
+          "pitch": -0.08440038162082075,
+          "title": "Torfschicht",
+          "text": "Hier kann man gut erkennen, wie mächtig die Torschicht einmal war. Schaut ihr in die andere Richtung seht ihr, eine Wiese. Die Torfmächtigkeit ist hier etwa 2 Meter. an anderen Standorten konnte die Torfschicht bis zu 12 Meter dick sein.&nbsp;&nbsp;"
+        },
+        {
+          "yaw": 1.8905678675704722,
+          "pitch": 0.0037154063596958053,
+          "title": "Grünland<div><br></div>",
+          "text": "Nach dem Abtorfen wurde der Boden gedüngt, so dass heute Grünfutter oder andere Nutzpflanzenangebaut werden können. Ursprünglich ist dasMoor sehr nährstoff arm. Deswegen musste es kultiviert werden, umes zum Anbau von Nutzpflanzen nutzbar zu machen."
+        },
+        {
+          "yaw": 0.668055792674533,
+          "pitch": -0.13889093574034383,
+          "title": "Birken im Moor?",
+          "text": "Birken habe im Moor nichts zu suchen. Eigentlich wachsen sie dort auch nicht, da es dort viel zu feucht für sie ist. Um Moorflächen nutzen zu können haben Menschen Gräben gezogen, so dass das Moor entwässert wurde. Erst dann konnten sich dort Birken ansiedeln. Sie verstärken die Entwässerung, da sie im Sommer viel Wasser über ihre Blätter verdunsten, das sie mit ihren Wurzeln aus dem Boden aufnehmen."
         }
       ]
+    },
+    {
+      "id": "4-torfwallweg",
+      "name": "TorfwallWeg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9365416136767966,
+          "pitch": 0.13592717911784824,
+          "rotation": 0,
+          "target": "2-torfwall"
+        },
+        {
+          "yaw": 0.18397867993859052,
+          "pitch": 0.0028039208181329656,
+          "rotation": 14.137166941154074,
+          "target": "0-standpunkt-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-birke",
+      "name": "Birke",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7711953643605804,
+          "pitch": 0.33307357521949754,
+          "rotation": 5.497787143782138,
+          "target": "1-schutzhtte"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wasser",
+      "name": "Wasser",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2406296100064385,
+          "pitch": -0.21223966439336195,
+          "rotation": 5.497787143782138,
+          "target": "0-standpunkt-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.755395128119229,
+          "pitch": 0.5038338356007515,
+          "title": "Wasserfläche<div><br></div>",
+          "text": "Das Wasser ist sehr nähsrstoffarm. Deshalb wachsen die Pflanztenarten auch nur relativ langsam im Vergleich zu nährstoffereichen Gewässern. Oft ist es bräunlich gefärbt. Das kommt von den Stoffen, die in den Torfmosses enthalten sind, den Huminsäuren."
+        }
+      ]
+    },
+    {
+      "id": "7-renaturierung",
+      "name": "Renaturierung",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9043984985549827,
+          "pitch": -0.13281802278745758,
+          "rotation": 0,
+          "target": "2-torfwall"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7479593340418695,
+          "pitch": 0.3944867237219256,
+          "title": "Renaturierung",
+          "text": "Hier kann man erkennen, wie das Moor langsam wieder seinen ursprünglichen Zustand einnimmt. Die Torfmoose wachsen immer weiter, die freien Wasserflächen werden immer kleiner.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "8-verbindungsweg",
+      "name": "Verbindungsweg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13255155376996264,
+          "pitch": -0.0906137221272445,
+          "rotation": 1.5707963267948966,
+          "target": "0-standpunkt-1"
+        },
+        {
+          "yaw": -2.9663807394098693,
+          "pitch": 0.2187090541467107,
+          "rotation": 10.995574287564278,
+          "target": "4-torfwallweg"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
-  "name": "WildesMoor",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
     "viewControlButtons": true
   }
